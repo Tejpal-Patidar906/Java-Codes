6b7812712eca5e61a89dc618e43c7b8c5e730e22
Tejpal <tejpalnathjipatidar906@gmail.com>
2026-06-19 20:14:12 +0530

    Adding Chocola problem code

--- GreedyALgo.java
+++ GreedyALgo.java
@@ -144,9 +144,43 @@ public class GreedyALgo {
         }
     }
 
+    public static void chocola(){
+        int n = 4, m = 6;
+        Integer costVer[] = {2,1,3,1,4}; //m-1
+        Integer costHor[] = {4 , 1 , 2}; // n-1
+        
+        Arrays.sort(costVer , Comparator.reverseOrder());
+        Arrays.sort(costHor , Comparator.reverseOrder());
+
+        int h = 0 , v=0;
+        int hp = 1 , vp = 1 , cost = 0;
+        
+        while(h < costHor.length && v < costVer.length){  
+            if(costVer[v] <= costHor[h]){    //HoriZontal cut maarna hai
+                cost += (costHor[h] * vp);
+                hp++;
+                h++;
+            }else{    //Vertical cut maarna hai
+                cost += (costVer[v] * hp);
+                vp++;
+                v++;
+            }
+        }
+        while(h < costHor.length){
+            cost += (costHor[h] * vp);
+            hp++;
+            h++;
+        }
+        while(v < costVer.length){
+            cost += (costVer[v] * hp);
+            vp++;
+            v++;
+        }
+        System.out.println("Min. Cost = " + cost);
+    }
+
 
     public static void main(String[] args) {
-        int jobsInfo[][] = {{4,20} , {1,10} , {1,40} , {1,30}};
-        jobSequence(jobsInfo);
+        chocola();
     }
 }
