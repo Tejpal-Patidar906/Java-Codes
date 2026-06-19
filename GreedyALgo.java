c215c99f2fa0843933a776e10338d72bd76e77a9
Tejpal <tejpalnathjipatidar906@gmail.com>
2026-06-19 19:07:19 +0530

    Greedy Algo. Codes Commit

--- GreedyALgo.java
+++ GreedyALgo.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 public class GreedyALgo {
 
@@ -37,9 +35,118 @@ public class GreedyALgo {
         }
         System.out.println();
     }
+
+    public static void fractionalKnapsack(int weight[] , int value[] , int w){
+        double ratio[][] = new double[weight.length][2];
+        for(int i=0; i<value.length; i++){
+            ratio[i][0] = i;
+            ratio[i][1] = value[i] / (double) weight[i];
+        }
+
+        Arrays.sort(ratio , Comparator.comparingDouble(o -> o[1]));
+
+        double val = 0;
+        int capacity = w;
+
+        for(int i=ratio.length-1; i>=0; i--){
+            int idx = (int) ratio[i][0];
+            if(capacity >= weight[idx]){
+                val = val + value[idx];
+                capacity = capacity  - weight[idx];
+            }else{
+                val = val + (capacity * ratio[i][1]);
+                 capacity = 0;
+                break;
+            }
+        }
+        System.out.println("Answer = " + val);
+    }
+
+    public static void minDiff(int A[] , int B[]){
+         Arrays.sort(A);
+         Arrays.sort(B);
+         int minDiff = 0;
+         for(int i=0; i<A.length; i++){
+             minDiff += Math.abs(A[i] - B[i]);
+         }
+         System.err.println("Minimum Difference = " + minDiff);
+    }
+
+    public static void maxLength(int pairs[][]){
+         int n = pairs.length;
+         Arrays.sort(pairs , Comparator.comparingDouble(o -> o[1]));
+         int ans = 1;
+         int chainend = pairs[0][1];
+         for(int i=1; i<n; i++){
+            if(pairs[i][0] > chainend){
+                ans++;
+                chainend = pairs[i][1];
+            }
+         }
+         System.out.println("ans = " + ans);
+    }
+
+    public static void indianCoins(int amt){
+        int arr[] = {2000 , 500 , 100 , 50 , 20 , 10 , 5 , 2 , 1};
+        int n = arr.length;
+        int count = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            if(arr[i] < amt){
+                while(arr[i] <= amt){
+                    count++;
+                    list.add(arr[i]);
+                    amt -= arr[i];
+                }
+            }
+        } 
+        System.out.println(list);
+        System.out.println("Ans = " + count);
+    }
+
+    
+    
+
+    public static void jobSequence(int jobsInfo[][]){
+
+        ArrayList<Job> jobs = new ArrayList<>();
+        
+        for(int i=0; i<jobsInfo.length; i++){
+            jobs.add(new Job(i, jobsInfo[i][0] , jobsInfo[i][1]));
+        }
+
+        Collections.sort(jobs , (a,b) -> b.profit - a.profit);   //descending order of profit       // Lambda function() -> (a,b) -> b.profit - a.profit  
+        int time = 0;
+
+        ArrayList<Integer> seq = new ArrayList<>();
+
+        for(int i=0; i<jobs.size(); i++){
+            Job curr = jobs.get(i);
+            if(curr.deadLine > time){
+                 seq.add(curr.id);
+                 time++;
+            }
+        }
+
+        System.out.println("Max Jobs = " + seq.size());
+        System.out.println(seq);
+    }
+
+    static class Job{
+        int deadLine;
+        int profit;
+        int id;
+
+        public Job(int i , int d , int p){
+            id = i;
+            deadLine = d;
+            profit = p;
+        }
+    }
+
+
     public static void main(String[] args) {
-        int start[] = {1,3,0,5,8,5};
-        int end[] = {2,4,6,7,9,9};
-        maxAct(start, end);
+        int jobsInfo[][] = {{4,20} , {1,10} , {1,40} , {1,30}};
+        jobSequence(jobsInfo);
     }
 }
