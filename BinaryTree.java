55dff4d7846d4f81c2ad586114916c2bc99e4a28
Tejpal <tejpalnathjipatidar906@gmail.com>
2026-06-25 19:34:29 +0530

    Binary trees part 3

--- BinaryTree.java
+++ BinaryTree.java
@@ -32,9 +32,7 @@ public class BinaryTree{
             this.hd = hd;
         }
     }
-
-
-
+ 
     static class BinaryTreee{
         static int idx = -1;
         public static Node buildTree(int nodes[]){
@@ -232,14 +230,148 @@ public class BinaryTree{
             }
             System.out.println();
         }
+
+        public static void kLevel(Node root , int level , int k){
+            if(root == null){
+                return;
+            }
+            if(level == k){
+                System.out.print(root.data + " ");
+                return;
+            }
+            kLevel(root.left, level+1, k);
+            kLevel(root.right, level+1, k);
+        }
+
+        public static boolean getPath(Node root , int n , ArrayList<Node> path){
+            if(root == null){
+                return false;
+            }
+            path.add(root);
+
+            if(root.data == n){
+                return true;
+            }
+            boolean foundLeft = getPath(root.left, n, path);
+            boolean foundRight = getPath(root.right, n, path);
+
+            if(foundLeft || foundRight){
+                return true;
+            }
+
+            path.remove(path.size()-1);
+            
+            return false;
+        }
+
+        public static Node lca(Node root , int n1 , int n2){
+            ArrayList<Node> path1 = new ArrayList<>();
+            ArrayList<Node> path2 = new ArrayList<>();
+
+            getPath(root , n1 , path1);
+            getPath(root , n2 , path2);
+
+            int i=0;
+            for(; i<path1.size() && i<path2.size(); i++){
+                if(path1.get(i) != path2.get(i)){
+                    break;
+                }
+            }
+            return path1.get(i-1);
+        }
+
+        public static Node lca2(Node root , int n1 , int n2){
+            if(root == null){
+                return null;
+            }
+            if(root.data == n1 || root.data == n2){
+                return root;
+            }
+            Node leftLca = lca2(root.left, n1, n2);
+            Node rightLca = lca2(root.right, n1, n2);
+
+            if(leftLca == null){
+                return rightLca;
+            }
+            if(rightLca == null){
+                return leftLca;
+            }
+            return root;
+        }
+
+        public static int lcaDist(Node root , int n){   
+            if(root == null){
+                return -1;
+            }
+            if(root.data == n){
+                return 0;
+            }
+            int leftDist = lcaDist(root.left, n);
+            int rightDist = lcaDist(root.right, n);
+
+            if(leftDist == -1 && rightDist == -1){
+                return -1;
+            }else if(leftDist == -1){
+                return rightDist+1;
+            }else{
+                return leftDist+1;
+            }
+        }
+
+        public static int minDist(Node root , int n1 , int n2){
+            Node lca = lca2(root, n1, n2);
+            int dist1 = lcaDist(lca , n1);
+            int dist2 = lcaDist(lca , n2);
+
+            return dist1 + dist2;
+        }
+
+        public static int kthAncestor(Node root , int k , int node){
+            if(root == null){
+                return -1;
+            }
+            if(root.data == node){
+                return 0;
+            }
+            int leftDist = kthAncestor(root.left, k, node);
+            int rightDist = kthAncestor(root.right, k, node);
+
+            if(leftDist == -1 && rightDist == -1){
+                return -1;
+            }
+            int max = Math.max(leftDist, rightDist);
+            if(max+1 == k){
+                System.out.println(root.data);
+            }
+            return max+1;
+        }
+
+        public static int transform(Node root){
+            if(root == null){
+                return 0;
+            }
+            
+            int leftChild = transform(root.left);
+            int rightChild = transform(root.right);
+
+            int data = root.data;
+            int newLeft = root.left == null ? 0 : root.left.data;
+            int newRight = root.right == null ? 0 : root.right.data;
+
+            root.data = newLeft + leftChild + newRight + rightChild;
+
+            return data;
+        }
     }
 
+
     public static void main(String[] args) {
-        int nodes[] = {1,2,-1,4,-1,5,-1,6,-1,-1,3,-1,-1};
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,6,-1,-1,7,-1,-1};
         //int nodes2[] = {2,4,-1,-1,5,-1,-1};
         BinaryTreee tree = new BinaryTreee();
         tree.idx = -1;
         Node root = tree.buildTree(nodes);
-        tree.topView(root);
+        tree.transform(root);
+        tree.preOrder(root);
     }
 }
